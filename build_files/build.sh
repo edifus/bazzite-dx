173dd5cc888fab68c741305a94a066d5d564b694
edifus <edifus@gmail.com>
2025-05-29 08:00:19 -0600

    debug comments

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,14 @@ export CONTEXT_PATH
 export SCRIPTS_PATH
 export MAJOR_VERSION_NUMBER
 
+echo "CONTEXT_PATH:          $CONTEXT_PATH"
+echo "BUILD_SCRIPTS_PATH:    $BUILD_SCRIPTS_PATH"
+echo "MAJOR_VERSION_NUMBER:  $MAJOR_VERSION_NUMBER"
+echo "SCRIPTS_PATH:          $SCRIPTS_PATH"
+
 run_buildscripts_for() {
 	WHAT=$1
+	echo "WHAT: $SHAT"
 	shift
 	# Complex "find" expression here since there might not be any overrides
 	# Allows us to numerically sort scripts by stuff like "01-packages.sh" or whatever
@@ -20,6 +26,7 @@ run_buildscripts_for() {
 		if [ "${CUSTOM_NAME}" != "" ] ; then
 			WHAT=$CUSTOM_NAME
 		fi
+		echo "WHAT: $WHAT"
 		printf "::group:: ===$WHAT-%s===\n" "$(basename "$script")"
 		"$(realpath $script)"
 		printf "::endgroup::\n"
@@ -28,17 +35,20 @@ run_buildscripts_for() {
 
 copy_systemfiles_for() {
 	WHAT=$1
+	echo "WHAT: $WHAT"
 	shift
 	DISPLAY_NAME=$WHAT
+	echo "DISPLAY_NAME: $DISPLAY_NAME"
 	if [ "${CUSTOM_NAME}" != "" ] ; then
 		DISPLAY_NAME=$CUSTOM_NAME
 	fi
+	echo "DISPLAY_NAME: $DISPLAY_NAME"
 	printf "::group:: ===%s-file-copying===\n" "${DISPLAY_NAME}"
 	cp -avf "${CONTEXT_PATH}/$WHAT/." /
 	printf "::endgroup::\n"
 }
 
 CUSTOM_NAME="base"
-copy_systemfiles_for system
+copy_systemfiles_for files
 run_buildscripts_for .
 CUSTOM_NAME=
